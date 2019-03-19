50ba2112a4f2aa37a9e2a8a1fb5eaf21883047e8
sakura <975976959@qq.com>
2019-03-19 18:08:10 +0800

    upup

--- app/src/main/java/sakura/com/lejinggou/Activity/PriceDetailsActivity.java
+++ app/src/main/java/sakura/com/lejinggou/Activity/PriceDetailsActivity.java
@@ -145,10 +145,15 @@ public class PriceDetailsActivity extends BaseActivity implements View.OnClickLi
                 if (goodsIndexBean.getData().getType() == 2) {
                     int s = goodsIndexBean.getData().getS();
                     if (s <= 0) {
-                        startActivity(new Intent(context, MaiChangReGouActivity.class)
-                                .putExtra("id", goodsIndexBean.getData().getId())
-                                .putExtra("title", goodsIndexBean.getData().getName())
-                        );
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                startActivity(new Intent(context, MaiChangReGouActivity.class)
+                                        .putExtra("id", goodsIndexBean.getData().getId())
+                                        .putExtra("title", goodsIndexBean.getData().getName())
+                                );
+                            }
+                        },1000);
                     } else {
                         startActivity(new Intent(context, MaiChangReGouActivity.class)
                                 .putExtra("id", goodsIndexBean.getData().getId())
