252ee0960116ccb31e6ef9c31ff91b080db05ad6
sakura <975976959@qq.com>
2019-12-05 18:09:05 +0800

    upup

--- app/src/main/java/sakura/com/lejinggou/Activity/PriceDetailsActivity.java
+++ app/src/main/java/sakura/com/lejinggou/Activity/PriceDetailsActivity.java
@@ -136,6 +136,13 @@ public class PriceDetailsActivity extends BaseActivity implements View.OnClickLi
                     return;
                 }
 
+                long l = Long.parseLong(goodsIndexBean.getData().getStarttime());
+                long l1 = System.currentTimeMillis() / 1000;
+                if (l<=l1){
+                    EZToast.showShort(context, "商品预购中");
+                    return;
+                }
+
                 if (goodsIndexBean.getData().getType() == 1) {
                     startActivity(new Intent(context, MaiChangReGouActivity.class)
                             .putExtra("id", goodsIndexBean.getData().getId())
